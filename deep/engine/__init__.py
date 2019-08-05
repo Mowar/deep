a16aaa5eba1108a5456693d062f34e14192ccdb7
Mowar <476900563@qq.com>
2019-08-05 21:38:11 +0800

    添加keras初始代码

---
+++ deep/engine/__init__.py
@@ -0,0 +1,7 @@
+# note: `Node` is an internal class,
+# it isn't meant to be used by Keras users.
+from .input_layer import Input
+from .input_layer import InputLayer
+from .base_layer import InputSpec
+from .base_layer import Layer
+from .training import Model
